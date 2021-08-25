45face36a20388a75c63cb2c8d8c25f2e8d58352
Roman <r.kovtunovych@psiphon.ca>
2021-08-25 17:49:52 -0400

    added email notifier

--- host_probe.py
+++ host_probe.py
@@ -6,13 +6,18 @@ def str_to_class(classname):
     return getattr(sys.modules[__name__], classname)
 
 FILE = 'subscriptions.csv'
+
+sender = ''
+password = ''
+receiver = ''
+
 reader = {}
 with open(FILE) as csvfile:
   reader = csv.DictReader(csvfile)
 
   for row in reader:
     probe = str_to_class(row['class'])(row['url'])
-    probe.setNotifyOnError(LogNotifier())
+    probe.setNotifyOnError(EmailNotifier('smtp.gmail.com','465',sender,password,receiver))
     probe.run()
 
 #probe = TLSExpiryProbe('/tmp/apache-selfsigned.crt')
--- notifiers/notifier.py
+++ notifiers/notifier.py
@@ -1,4 +1,4 @@
-import logging
+import logging, ssl, smtplib
 class Notifier():
   def notify(self, msg):
     print("Notification: " + msg)
@@ -6,4 +6,29 @@ class Notifier():
 class LogNotifier(Notifier):
   def notify(self, msg):
     logging.error(msg)
+
+class EmailNotifier(Notifier):
+  server_addr = None
+  port = None
+  sender = None
+  password = None
+  receiver = None
+
+  def __init__(self,server_addr,port,sender,password,receiver):
+    self.server_addr = server_addr
+    self.receiver = receiver
+    self.port = port
+    self.sender = sender
+    self.password = password
+
+  def notify(self, msg):
+    context = ssl.create_default_context()
+    message = """\
+    Subject: Probe Fail
+
+    """ + msg
+    with smtplib.SMTP_SSL(self.server_addr, self.port, context=context) as server:
+      server.login(self.sender, self.password)
+      server.sendmail(self.receiver,self.receiver,message)
+
     
